2ec0872492c395855e06cf2ac52a907834f44631
prathameshpm <mahamulkarprathamesh@gmail.com>
2017-09-03 21:30:40 +0530

    patched to earlier commit of Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
+var configValues = require('./config.json');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/ToDoApp');
+mongoose.connect(`mongodb://${configValues.username}:${configValues.password}@ds121494.mlab.com:21494/node-todo-api-am` || 'mongodb://localhost:27017/ToDoApp');
 
 module.exports = {mongoose};
