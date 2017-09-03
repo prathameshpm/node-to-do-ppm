09dfa14f3878f0b61fe24b335f8aaeb21640c0df
prathameshpm <mahamulkarprathamesh@gmail.com>
2017-09-03 21:36:33 +0530

    Patched config file error solution to previous commit Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var configValues = require('./config.json');
+var configValues = require('./config');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(`mongodb://${configValues.username}:${configValues.password}@ds121494.mlab.com:21494/node-todo-api-am` || 'mongodb://localhost:27017/ToDoApp');
