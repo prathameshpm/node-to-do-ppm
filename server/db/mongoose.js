b34b49f1e0c94a8526d31bf33a3389b8d2af39fe
prathameshpm <mahamulkarprathamesh@gmail.com>
2017-09-07 12:14:37 +0530

    Removed sensitive info from code

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,6 @@
 var mongoose = require('mongoose');
-// var configValues = require('./config');
 
 mongoose.Promise = global.Promise;
-// mongoose.connect(database = `mongodb://${configValues.username}:${configValues.password}@ds121494.mlab.com:21494/node-todo-api-am`);
 
 mongoose.connect(process.env.PROD_MONGODB);
 
